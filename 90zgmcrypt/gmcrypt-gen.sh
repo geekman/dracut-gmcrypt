9c3cc76940026dfea7c198ac41aceb90c545ad31
Darell Tan <darell.tan@gmail.com>
2014-11-07 23:05:54 +0800

    Updated runtime scripts to support newer dracut version used by CentOS 7.

--- 90zgmcrypt/gmcrypt-gen.sh
+++ 90zgmcrypt/gmcrypt-gen.sh
@@ -2,9 +2,17 @@
 
 . /lib/dracut-gmcrypt-lib.sh
 
+NO_LUKS=0
 LUKS=$(getargs rd_LUKS_UUID=)
+getarg rd_NO_LUKS && NO_LUKS=1
 
-if ! getarg rd_NO_LUKS && [ -n "$LUKS" ] ; then
+# dracut >= 033
+if type getargbool >/dev/null 2>&1; then
+    LUKS=$(getargs rd.luks.uuid -d rd_LUKS_UUID)
+    getargbool 1 rd.luks -d -n rd_NO_LUKS || NO_LUKS=1
+fi
+
+if [ $NO_LUKS -eq 0 ] && [ -n "$LUKS" ] ; then
     gmcrypt_genkey /tmp/rootfs.key $LUKS
 fi
 
